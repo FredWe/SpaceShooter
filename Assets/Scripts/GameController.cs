513cce697a2d713c9c7c20232d53bf80820ba38b
FredWe <robert.wei.176@gmail.com>
2015-12-01 17:04:45 +0100

    finished Tutorial 11

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+	//1. spawning harzards
+
+	public GameObject harzard;
+	public Vector3 spawnValues;
+
+	void spawnWaves () {
+		Vector3 spawnPosition = new Vector3 (Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+		Quaternion spawnRotation = Quaternion.identity;
+		Instantiate (harzard, spawnPosition, spawnRotation);
+	}
+
+	// Use this for initialization
+	void Start () {
+		spawnWaves ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
