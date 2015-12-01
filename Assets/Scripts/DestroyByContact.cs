f340e7fd376c94d628044bbc7873640e2470e6b2
FredWe <robert.wei.176@gmail.com>
2015-12-01 16:22:39 +0100

    finished Tutorial 9

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other) {
+		//Debug.Log (other.name);
+		if (other.tag == "Boundary") {
+			return;
+		}
+		Destroy (other.gameObject);
+		Destroy (gameObject);
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
